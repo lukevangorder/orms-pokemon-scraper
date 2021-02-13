748c6a7ba184ea4835187f172971b5280edffd54
lukevangorder <lukevangorder@gmail.com>
2021-02-13 16:15:25 -0500

    commit tree ccbe42457c2b712ae308bdfe538ef1377c8b188f

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+    attr_reader :name, :type, :db, :id
+    def initialize(name:, type:, db:, id:nil)
+        @name = name
+        @type = type
+        @db = db
+        @id = id
+    end
+    def self.save(name, type, database)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        database.execute(sql, [name, type])
+        @id = database.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+    def self.find(id_number, database)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = #{id_number}
+        SQL
+        array = database.execute(sql)[0]
+        found = self.new(name: array[1], type: array[2], db: database, id: id_number)
+        return found
+    end
 end
